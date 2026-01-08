6dfa17423178c527dd04ee576ebb23b3d6ac9c0f
meetroj <meetroj441@gmail.com>
2026-01-08 14:27:22 +0530

    Add test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is live 🚀");
+  res.send("Backend is live ");
 });
 
 const authRoutes = require('./src/routes/authRoutes');
