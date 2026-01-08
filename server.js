107c40611766a9f004f262e953cc80a6819773c1
meetroj <meetroj441@gmail.com>
2026-01-08 12:26:31 +0530

    Add server

--- server.js
+++ server.js
@@ -2,23 +2,25 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const connectDB = require('./src/config/db');
-connectDB();
-
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is live 🚀");
 });
 
-app.use(express.json());
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 const authRoutes = require('./src/routes/authRoutes');
 app.use('/api/auth', authRoutes);
+
+const connectDB = require('./src/config/db');
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error("DB connection failed:", err.message);
+});
